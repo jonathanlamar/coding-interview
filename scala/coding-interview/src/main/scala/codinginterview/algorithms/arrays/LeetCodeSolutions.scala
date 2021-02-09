7be56096959d67fd7b0ad827d21b0440ace74df1
Jon <jonathan.p.lamar@gmail.com>
2021-02-09 16:02:55 -0700

    More studying

--- scala/coding-interview/src/main/scala/codinginterview/algorithms/arrays/LeetCodeSolutions.scala
+++ scala/coding-interview/src/main/scala/codinginterview/algorithms/arrays/LeetCodeSolutions.scala
@@ -2,6 +2,7 @@ package codinginterview.algorithms.arrays
 
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.Stack
+import scala.math.min
 
 object LeetCodeSolutions {
   def validParens(parenString: String): Boolean = {
@@ -26,5 +27,88 @@ object LeetCodeSolutions {
     }
 
     return openParenStack.isEmpty
+  }
+
+  /** https://leetcode.com/problems/longest-palindromic-substring/
+    * Given a string s, return the longest palindromic substring in s.
+    */
+  def longestPalindrome(str: String): String = {
+    val n = str.length()
+
+    // Considering the letters and spaces between them as an array
+    // of length 2n-1, this finds the maximal palindrome centered at node i
+    def expandAroundCenter(i: Int): (String, Int) = {
+      var currentLen = i % 2 // 1 if contains letter, else 0
+      var centerPoint = n.toDouble / 2
+      var le = centerPoint.toInt
+      var re = (centerPoint + 0.5).toInt
+      var pastLe = 0
+      var pastRe = 0
+
+      while (str(le) == str(re) && le >= 0 && re < n) {
+        currentLen += 2
+        pastLe = le
+        pastRe = re
+        le -= 1
+        re += 1
+      }
+
+      return (str.substring(pastLe, pastRe + 1), currentLen)
+    }
+
+    var bestLen = 0
+    var bestPalindrome = ""
+
+    for (i <- 0 until 2 * n - 1) {
+      val (palindrome, newLen) = expandAroundCenter(i)
+      if (newLen > bestLen) {
+        bestLen = newLen
+        bestPalindrome = palindrome
+      }
+    }
+
+    return bestPalindrome
+  }
+
+  /** https://leetcode.com/problems/container-with-most-water/
+    * Given n non-negative integers a1, a2, ..., an , where each represents a
+    * point at coordinate (i, ai). n vertical lines are drawn such that the two
+    * endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which,
+    * together with the x-axis forms a container, such that the container
+    * contains the most water.  Notice that you may not slant the container.
+    */
+  def maxArea(arr: List[Int]): (Int, Int, Int) = {
+    var bestArea = 0
+    var bestLe = 0
+    var bestRe = arr.length - 1
+    var le = 0
+    var re = arr.length - 1
+
+    while (le < re) {
+
+      val ly = arr(le)
+      val ry = arr(re)
+      if (ly <= ry) {
+        val area = ly * (re - le)
+        if (area > bestArea) {
+          bestArea = area
+          bestLe = le
+          bestRe = re
+        }
+
+        le += 1
+      } else {
+        val area = ry * (re - le)
+        if (area > bestArea) {
+          bestArea = area
+          bestLe = le
+          bestRe = re
+        }
+
+        re -= 1
+      }
+    }
+
+    return (bestArea, bestLe, bestRe)
   }
 }
--- scala/coding-interview/src/main/scala/codinginterview/algorithms/linkedlists/LeetCodeSolutions.scala
+++ scala/coding-interview/src/main/scala/codinginterview/algorithms/linkedlists/LeetCodeSolutions.scala
@@ -3,6 +3,63 @@ package codinginterview.algorithms.linkedlists
 import codinginterview.datastructures.LinkedList._
 
 object LeetCodeSolutions {
+
+  /** https://leetcode.com/problems/add-two-numbers/
+    * You are given two non-empty linked lists representing two non-negative
+    * integers. The digits are stored in reverse order, and each of their nodes
+    * contains a single digit. Add the two numbers and return the sum as a
+    * linked list.
+    *
+    * You may assume the two numbers do not contain any leading zero, except
+    * the number 0 itself.
+    */
+  def addTwoNumbers_WRONG(
+      l1: LinkedListNode,
+      l2: LinkedListNode
+  ): LinkedListNode = {
+
+    // Stack safe version of the tail recursion.
+    def innerLoop(
+        l1: LinkedListNode,
+        l2: LinkedListNode,
+        accumulator: LinkedListNode,
+        carry: Int
+    ): LinkedListNode = (l1, l2) match {
+      case (EmptyNode, EmptyNode) =>
+        if (carry > 0) accumulator.insertAtTail(carry) else accumulator
+      case (EmptyNode, b: SinglyLinkedNode) =>
+        innerLoop(SinglyLinkedNode(carry), b, accumulator, 0)
+      case (a: SinglyLinkedNode, EmptyNode) =>
+        innerLoop(a, SinglyLinkedNode(carry), accumulator, 0)
+      case (a: SinglyLinkedNode, b: SinglyLinkedNode) => {
+        var partSum = carry + a.value + b.value
+        innerLoop(
+          a.next,
+          b.next,
+          accumulator.insertAtTail(partSum % 10),
+          partSum / 10
+        )
+      }
+    }
+
+    return innerLoop(l1, l2, EmptyNode, 0)
+  }
+
   def addTwoNumbers(l1: LinkedListNode, l2: LinkedListNode): LinkedListNode =
-    ???
+    (l1, l2) match {
+      case (EmptyNode, EmptyNode) => EmptyNode
+      case (EmptyNode, _)         => l2
+      case (_, EmptyNode)         => l1
+
+      // Generic case
+      case (SinglyLinkedNode(val1, next1), SinglyLinkedNode(val2, next2)) => {
+        if (val1 + val2 < 10)
+          SinglyLinkedNode(val1 + val2, addTwoNumbers(next1, next2))
+        else
+          SinglyLinkedNode(
+            val1 + val2 - 10,
+            addTwoNumbers(addTwoNumbers(SinglyLinkedNode(1), next1), next2)
+          )
+      }
+    }
 }
